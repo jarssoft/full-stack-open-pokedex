ffb19d54555dc0069df2021bd80b2df0cc28fe10
Jari Saari <jari.m.saari@gmail.com>
2024-08-12 19:07:42 +0300

    11.12. test1

--- app.js
+++ app.js
@@ -11,5 +11,9 @@ app.listen(PORT, () => {
 })
 
 app.get('/version', (req, res) => {
-  res.send('test3') // change this string to ensure a new version deployed
+  res.send('test1') // change this string to ensure a new version deployed
+})
+
+app.get('/health', (req, res) => {
+  res.send('ok')
 })
