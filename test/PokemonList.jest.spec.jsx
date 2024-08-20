1742a811d4ebe819ab1d123d68b9a49b04f517f1
Jari Saari <jari.m.saari@gmail.com>
2024-08-21 00:20:54 +0300

    fake error in test, not in deploy

--- test/PokemonList.jest.spec.jsx
+++ test/PokemonList.jest.spec.jsx
@@ -22,7 +22,7 @@ describe('<PokemonList />', () => {
         <PokemonList pokemonList={pokemonList} />
       </BrowserRouter>
     )
-    expect(screen.getByText('bulbasaur')).toBeVisible()
+    expect(screen.getByText('bulbasaur-fake-error')).toBeVisible()
     expect(screen.getByText('eevee')).toBeVisible()
   })
 })
